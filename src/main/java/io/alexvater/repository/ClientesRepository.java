128d8caf1dfae73309b3a07043bb2ff857fb66ee
AlexVater <alexsander.vater@gmail.com>
2021-09-08 17:52:34 -0300

    primeiro commit

---
+++ src/main/java/io/alexvater/repository/ClientesRepository.java
@@ -0,0 +1,11 @@
+package io.alexvater.repository;
+
+import io.alexvater.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente) {
+        //acessa a base e salva o cliente
+    }
+}
---
+++ src/main/java/io/alexvater/service/ClientesService.java
@@ -0,0 +1,25 @@
+package io.alexvater.service;
+
+import io.alexvater.model.Cliente;
+import io.alexvater.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientesService {
+
+    private ClientesRepository repository;
+
+    public ClientesService( ClientesRepository repository){
+        this.repository = repository;
+    }
+
+    public void salvarCliente(Cliente cliente){
+        validarCliente(cliente);
+        this.repository.persistir(cliente);
+    }
+
+    public void validarCliente(Cliente cliente){
+        //aplicar validações
+    }
+}
