b227691bd4975bdeba18e8dd0d6e2685c051b7a1
AlexVater <alexsander.vater@gmail.com>
2021-09-09 16:34:00 -0300

    anotations

--- src/main/java/io/alexvater/MyConfiguration.java
+++ src/main/java/io/alexvater/MyConfiguration.java
@@ -1,13 +1,15 @@
 package io.alexvater;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Development
 public class MyConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName() {
-        return "Sistema de Vendas";
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("rodando a config de dev");
+        };
     }
 }
